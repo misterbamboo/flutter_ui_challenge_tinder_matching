489658d7dd1df6969c3f0c4f1ac27e9a48ad2f04
Matt Carroll <me@mattcarroll.name>
2018-06-11 23:24:27 -0700

    1. Static Layout

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
+        primaryColorBrightness: Brightness.light,
         primarySwatch: Colors.blue,
       ),
       home: new MyHomePage(),
@@ -21,13 +22,148 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Widget _buildAppBar() {
+    return new AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+      leading: new IconButton(
+        icon: new Icon(
+          Icons.person,
+          color: Colors.grey,
+          size: 40.0,
+        ),
+        onPressed: () {
+          // TODO:
+        },
+      ),
+      title: new FlutterLogo(
+        size: 30.0,
+        colors: Colors.red,
+      ),
+      actions: <Widget>[
+        new IconButton(
+          icon: new Icon(
+            Icons.chat_bubble,
+            color: Colors.grey,
+            size: 40.0,
+          ),
+          onPressed: () {
+            // TODO:
+          },
+        )
+      ],
+    );
+  }
+
+  Widget _buildBottomBar() {
+    return BottomAppBar(
+      color: Colors.transparent,
+      elevation: 0.0,
+      child: new Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            new RoundIconButton.small(
+              icon: Icons.refresh,
+              iconColor: Colors.orange,
+              onPressed: () {
+                // TODO:
+              },
+            ),
+            new RoundIconButton.large(
+              icon: Icons.clear,
+              iconColor: Colors.red,
+              onPressed: () {
+                // TODO:
+              },
+            ),
+            new RoundIconButton.small(
+              icon: Icons.star,
+              iconColor: Colors.blue,
+              onPressed: () {
+                // TODO:
+              },
+            ),
+            new RoundIconButton.large(
+              icon: Icons.favorite,
+              iconColor: Colors.green,
+              onPressed: () {
+                // TODO:
+              },
+            ),
+            new RoundIconButton.small(
+              icon: Icons.lock,
+              iconColor: Colors.purple,
+              onPressed: () {
+                // TODO:
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(''),
-      ),
+      appBar: _buildAppBar(),
       body: new Center(),
+      bottomNavigationBar: _buildBottomBar(),
+    );
+  }
+}
+
+class RoundIconButton extends StatelessWidget {
+  final IconData icon;
+  final Color iconColor;
+  final double size;
+  final VoidCallback onPressed;
+
+  RoundIconButton.large({
+    this.icon,
+    this.iconColor,
+    this.onPressed,
+  }) : size = 60.0;
+
+  RoundIconButton.small({
+    this.icon,
+    this.iconColor,
+    this.onPressed,
+  }) : size = 50.0;
+
+  RoundIconButton({
+    this.icon,
+    this.iconColor,
+    this.size,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      width: size,
+      height: size,
+      decoration: new BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.white,
+        boxShadow: [
+          new BoxShadow(
+            color: const Color(0x11000000),
+            blurRadius: 10.0,
+          ),
+        ],
+      ),
+      child: new RawMaterialButton(
+        shape: new CircleBorder(),
+        elevation: 0.0,
+        child: new Icon(
+          icon,
+          color: iconColor,
+        ),
+        onPressed: onPressed,
+      ),
     );
   }
 }
